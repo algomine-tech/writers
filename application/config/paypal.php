3676a0ae5a74e09a531bc4ad9cdb9b7ba044c01e
oscarsalu <oscarsalu@gmail.com>
2017-04-11 17:01:49 +0300

    pay pall

--- application/config/paypal.php
+++ application/config/paypal.php
@@ -1,70 +1,61 @@
-<?php
+<?php  
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-/**
+/** 
  * Sandbox / Test Mode
- * -------------------
- *
+ * -------------------------
  * TRUE means you'll be hitting PayPal's sandbox/test servers.  FALSE means you'll be hitting the live servers.
  */
 $config['Sandbox'] = TRUE;
 
-/**
+/* 
  * PayPal API Version
- * --------------------------
- *
- * The library is currently using PayPal API version 119.0.
+ * ------------------
+ * The library is currently using PayPal API version 123.0.
  * You may adjust this value here and then pass it into the PayPal object when you create it within your scripts to override if necessary.
  */
 $config['APIVersion'] = '123.0';
 
-/**
+/*
  * PayPal Gateway API Credentials
  * ------------------------------
- *
  * These are your PayPal API credentials for working with the PayPal gateway directly.
  * These are used any time you're using the parent PayPal class within the library.
- *
+ * 
  * We're using shorthand if/else statements here to set both Sandbox and Production values.
  * Your sandbox values go on the left and your live values go on the right.
+ * 
+ * You may obtain these credentials by logging into the following with your PayPal account.
+ * Sandbox: https://www.sandbox.paypal.com/us/cgi-bin/webscr?cmd=_login-api-run
+ * Live: https://www.paypal.com/us/cgi-bin/webscr?cmd=_login-api-run
  *
- * We have included our sandbox seller account credentials here so the demo will work right away.
- * If you have your own sandbox account you may simply replace these values with your own.
- *
- * You may obtain live credentials by logging into the following with your PayPal account:
- * https://www.paypal.com/us/cgi-bin/webscr?cmd=_login-api-run
  */
-$config['APIUsername'] = $config['Sandbox'] ? 'sandbo_1215254764_biz_api1.angelleye.com' : 'PRODUCTION_USERNAME_GOGES_HERE';
-$config['APIPassword'] = $config['Sandbox'] ? '1215254774' : 'PRODUCTION_PASSWORD_GOGES_HERE';
-$config['APISignature'] = $config['Sandbox'] ? 'AiKZhEEPLJjSIccz.2M.tbyW5YFwAb6E3l6my.pY9br1z2qxKx96W18v' : 'PRODUCTION_SIGNATURE_GOGES_HERE';
+$config['APIUsername'] = $config['Sandbox'] ? 'oscarsalu-facilitator_api1.gmail.com' : 'PRODUCTION_USERNAME_GOES_HERE';
+$config['APIPassword'] = $config['Sandbox'] ? 'VGLGDQZPK6CAGUWK' : 'PRODUCTION_PASSWORD_GOES_HERE';
+$config['APISignature'] = $config['Sandbox'] ? 'AFcWxV21C7fd0v3bYYYRCpSSRl31A3zmQrGeBJX9GDCHAAm.H7hCm04o' : 'PRODUCTION_SIGNATURE_GOES_HERE';
 
-/**
+/*
  * Payflow Gateway API Credentials
- *
- * NOTE: PayFlow credentials are only required for PayFlow specific demo kits.
- * -------------------------------
- *
+ * ------------------------------
  * These are the credentials you use for your PayPal Manager:  http://manager.paypal.com
  * These are used when you're working with the PayFlow child class.
- *
+ * 
  * We're using shorthand if/else statements here to set both Sandbox and Production values.
  * Your sandbox values go on the left and your live values go on the right.
- *
- * You may use the same credentials you use to login to your PayPal Manager,
+ * 
+ * You may use the same credentials you use to login to your PayPal Manager, 
  * or you may create API specific credentials from within your PayPal Manager account.
  */
-$config['PayFlowUsername'] = $config['Sandbox'] ? 'tester' : '';
-$config['PayFlowPassword'] = $config['Sandbox'] ? 'Pr0t3ct!' : '';
-$config['PayFlowVendor'] = $config['Sandbox'] ? 'angelleye' : '';
-$config['PayFlowPartner'] = $config['Sandbox'] ? 'PayPal' : 'PayPal';
+$config['PayFlowUsername'] = $config['Sandbox'] ? 'SANDBOX_USERNAME_GOES_HERE' : 'PRODUCTION_USERNAME_GOGES_HERE';
+$config['PayFlowPassword'] = $config['Sandbox'] ? 'SANDBOX_PASSWORD_GOES_HERE' : 'PRODUCTION_PASSWORD_GOES_HERE';
+$config['PayFlowVendor'] = $config['Sandbox'] ? 'SANDBOX_VENDOR_GOES_HERE' : 'PRODUCTION_VENDOR_GOES_HERE';
+$config['PayFlowPartner'] = $config['Sandbox'] ? 'SANDBOX_PARTNER_GOES_HERE' : 'PRODUCTION_PARTNER_GOES_HERE';
 
-/**
- * PayPal Adaptive Payments
- *
- * NOTE:  These settings are only required for Adaptive Payments specific demo kits.
- * ----------------------------------
- *
- * You obtain your application ID but submitting it for approval within your
+/*
+ * PayPal Application ID
+ * --------------------------------------
+ * The application is only required with Adaptive Payments applications.
+ * You obtain your application ID but submitting it for approval within your 
  * developer account at http://developer.paypal.com
  *
  * We're using shorthand if/else statements here to set both Sandbox and Production values.
@@ -72,15 +63,18 @@ $config['PayFlowPartner'] = $config['Sandbox'] ? 'PayPal' : 'PayPal';
  * The sandbox value included here is a global value provided for developrs to use in the PayPal sandbox.
  */
 $config['ApplicationID'] = $config['Sandbox'] ? 'APP-80W284485P519543T' : 'PRODUCTION_APP_ID_GOES_HERE';
-$config['DeviceID'] = '';
-$config['DeviceIpAddress'] = $_SERVER['REMOTE_ADDR'];
 
-/**
+/*
  * PayPal Developer Account Email Address
  * This is the email address that you use to sign in to http://developer.paypal.com
  */
-$config['DeveloperEmailAccount'] = 'some@email.com';
+$config['DeveloperEmailAccount'] = 'oscarsalu@gmail.com';
 
+/**
+ * Third Party User Values
+ * These can be setup here or within each caller directly when setting up the PayPal object.
+ */
+$config['DeviceID'] = 'DEVICE_ID_GOES_HERE';
 
-/* End of file paypal_sample.php */
-/* Location: ./system/application/config/paypal_sample.php */
+/* End of file paypal.php */
+/* Location: ./system/application/config/paypal.php */
